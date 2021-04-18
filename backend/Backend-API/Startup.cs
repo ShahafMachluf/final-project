d892198b3e841ae5f2bbe927b479f1795626aeb3
Shahaf machluf <45831619+ShahafMc@users.noreply.github.com>
2021-04-18 19:07:43 +0300

    test

--- backend/Backend-API/Startup.cs
+++ backend/Backend-API/Startup.cs
@@ -34,6 +34,7 @@ namespace Backend_API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt =>
