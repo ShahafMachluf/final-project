bf8d25414b2645efeaa6df52ba79d4364447b23b
gal kalash <galks@mta.ac.il>
2021-04-21 17:42:22 +0300

    f

--- backend/Backend-API/Models/Dog/CreateDogReqRes.cs
+++ backend/Backend-API/Models/Dog/CreateDogReqRes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,30 @@ namespace Backend_API.Models.Dog
     public class CreateDogReqRes
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public double Age { get; set; }
+        [Required]
+        public string Race { get; set; }
+        public eSize Size { get; set; }
+        [Required]
+        public eGender Gender { get; set; }
+        public string Information { get; set; }
+        public bool IsVaccinated { get; set; } //חיסון
+        public bool IsNeutered { get; set; } //סירוס
+
+        public enum eSize
+        {
+            Small,
+            Medium,
+            Large
+        }
+
+        public enum eGender
+        {
+            Male,
+            Female
+        }
         public bool Success { get; set; }
         public string Error { get; set; }
     }
