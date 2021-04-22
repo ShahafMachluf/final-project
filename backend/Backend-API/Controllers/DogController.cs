70eb746c360cb846182456966b0c6d04c0c19014
gal kalash <galks@mta.ac.il>
2021-04-22 13:08:03 +0300

    update DogController

--- backend/Backend-API/Controllers/DogController.cs
+++ backend/Backend-API/Controllers/DogController.cs
@@ -9,6 +9,7 @@ using System.Net;
 using Microsoft.AspNetCore.Http;
 using Backend_API.Models.User;
 using Backend_API.Services.Interfaces;
+using Backend_API.Models.Dog;
 
 namespace Backend_API.Controllers
 {
@@ -22,6 +23,41 @@ namespace Backend_API.Controllers
             IDogService dogService)
         {
             _dogService = dogService;
+        }
+
+        [HttpPost]
+        [Route("createDog")]
+        public async Task<IActionResult> createDog([FromBody] CreateDogReq request)
+        {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new CreateDogReqRes()
+                    {
+                        Error = "Invalid parameters",
+                        Success = false
+                    });
+                }
+
+                CreateDogReqRes result = await _dogService.CreateDogAsync(request);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException || ex is ApplicationException)
+                {
+                    return BadRequest(new CreateDogReqRes()
+                    {
+                        Success = false,
+                        Error = ex.Message
+                    });
+                }
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
