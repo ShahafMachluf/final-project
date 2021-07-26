f3da9446d579e84bb4faa81e4ab8c75c15722493
gal kalash <galks@mta.ac.il>
2021-07-26 20:33:55 +0300

    config update

--- frontend/App/app.config.js
+++ frontend/App/app.config.js
@@ -1,6 +1,6 @@
 export default {
     extra:{
-        APIAddress: "http://192.168.0.124:45455/api",
-        WebSocketAddress: "ws://192.168.0.124:45455"
+        APIAddress: "http://192.168.0.105:45455/api",
+        WebSocketAddress: "ws://192.168.0.105:45455"
     }
 };
